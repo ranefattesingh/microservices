248d01bab7c421fb51bcec320362c003a7654ae0
Fattesingh Rane <ranefattesingh@hotmail.com>
2026-07-15 21:58:11 +0530

    feat: detailed error module and response

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,9 +1,10 @@
 package router
 
 import (
-	"net/http"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/ranefattesingh/ecommerce-platform/pkg/response"
 )
 
 type Router interface {
@@ -19,15 +20,32 @@ type Route struct {
 	Name        string
 	Path        string
 	Method      string
-	HandlerFunc func(*gin.Context)
+	HandlerFunc HandlerFunc
+}
+
+type HandlerFunc func(*gin.Context) response.Responder
+
+func wrap(fn HandlerFunc) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		resp := fn(c)
+		if resp == nil {
+			return
+		}
+
+		fmt.Println(resp)
+
+		if err := resp.Respond(c); err != nil {
+
+		}
+	}
 }
 
 func NewV1Router(routers ...Router) *gin.Engine {
 	mux := gin.New()
 
-	mux.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"message": "pong"})
-	})
+	mux.GET("/ping", wrap(func(ctx *gin.Context) response.Responder {
+		return response.Ok(gin.H{"message": "pong"})
+	}))
 
 	mux.Use(gin.Logger())
 	mux.Use(gin.Recovery())
@@ -37,7 +55,7 @@ func NewV1Router(routers ...Router) *gin.Engine {
 	for _, router := range routers {
 		group := v1Group.Group(router.Routes().Name)
 		for _, route := range router.Routes().Routes {
-			group.Handle(route.Method, route.Path, route.HandlerFunc)
+			group.Handle(route.Method, route.Path, wrap(route.HandlerFunc))
 		}
 	}
 
