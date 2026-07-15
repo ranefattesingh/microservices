11b0cb1ef799f0fec8a6a60eb8d73a37a6dc3fc0
Fattesingh Rane <41385129+ranefattesingh@users.noreply.github.com>
2026-07-15 21:59:17 +0530

    feat: detailed error module and response (#6)

--- pkg/response/response.go
+++ pkg/response/response.go
@@ -1,82 +1,127 @@
 package response
 
 import (
-	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	httperror "github.com/ranefattesingh/ecommerce-platform/pkg/http_error"
 )
 
-// Response is the standard API envelope.
-type Response struct {
-	Success bool  `json:"success"`
-	Data    any   `json:"data,omitempty"`
-	Error   error `json:"error,omitempty"`
-	Meta    *Meta `json:"meta,omitempty"`
+type Responder interface {
+	Respond(c *gin.Context) error
 }
 
-type Meta struct {
-	Page       int `json:"page,omitempty"`
-	PerPage    int `json:"per_page,omitempty"`
-	Total      int `json:"total,omitempty"`
-	TotalPages int `json:"totalPages,omitempty"`
+type ResponseBuilder struct {
+	status  int
+	header  http.Header
+	cookies []*http.Cookie
+	body    any
 }
 
-func Ok(c *gin.Context, data any) {
-	c.JSON(http.StatusOK, Response{
-		Success: true,
-		Data:    data,
-	})
-}
+var _ Responder = (*ResponseBuilder)(nil)
 
-func Created(c *gin.Context, data any) {
-	c.JSON(http.StatusCreated, Response{
-		Success: true,
-		Data:    data,
-	})
-}
-
-func NoContent(c *gin.Context, data any) {
-	c.JSON(http.StatusNoContent, nil)
-}
-
-// ErrorResponse sends anappropriate error with status code.
-func ErrorResponse(c *gin.Context, err error) {
-	httpErr := new(httperror.ErrorInfo)
-
-	if errors.As(err, &httpErr) {
-		switch httpErr.HTTPStatusCode {
-		case http.StatusBadRequest:
-			BadRequest(c, err)
-		case http.StatusConflict:
-			Conflict(c, err)
-		default:
-			InternalServerError(c)
-		}
+func Response() *ResponseBuilder {
+	return &ResponseBuilder{
+		status:  http.StatusOK,
+		header:  http.Header{},
+		cookies: make([]*http.Cookie, 0),
 	}
 }
 
-// BadRequest sends an bad request error response.
-func BadRequest(c *gin.Context, err error) {
-	c.JSON(http.StatusBadRequest, Response{
-		Success: false,
-		Error:   err,
-	})
+func (rb *ResponseBuilder) Status(status int) *ResponseBuilder {
+	rb.status = status
+	return rb
 }
 
-// Conflict sends an bad request error response.
-func Conflict(c *gin.Context, err error) {
-	c.JSON(http.StatusConflict, Response{
-		Success: false,
-		Error:   err,
-	})
+func (rb *ResponseBuilder) Header(key string, values ...string) *ResponseBuilder {
+	for _, value := range values {
+		rb.header.Add(key, value)
+	}
+
+	return rb
 }
 
-// InternalServerError sends an error response.
-func InternalServerError(c *gin.Context) {
-	c.JSON(http.StatusInternalServerError, Response{
-		Success: false,
-		Error:   errors.New("server fail to handle the request"),
-	})
+func (rb *ResponseBuilder) Headers(headers http.Header) *ResponseBuilder {
+	for key, values := range headers {
+		for _, value := range values {
+			rb.header.Add(key, value)
+		}
+	}
+
+	return rb
+}
+
+func (rb *ResponseBuilder) Cookie(cookie *http.Cookie) *ResponseBuilder {
+	rb.cookies = append(rb.cookies, cookie)
+	return rb
+}
+
+func (rb *ResponseBuilder) Cookies(cookie []*http.Cookie) *ResponseBuilder {
+	rb.cookies = append(rb.cookies, cookie...)
+	return rb
+}
+
+func (rb *ResponseBuilder) Body(body any) *ResponseBuilder {
+	rb.body = body
+	return rb
+}
+
+func (rb *ResponseBuilder) ContentType(contentType string) *ResponseBuilder {
+	rb.header.Set("Content-Type", contentType)
+	return rb
+}
+
+func (rb *ResponseBuilder) Respond(c *gin.Context) error {
+	for key, values := range rb.header {
+		for _, value := range values {
+			c.Writer.Header().Add(key, value)
+		}
+	}
+
+	for _, cookie := range rb.cookies {
+		http.SetCookie(c.Writer, cookie)
+	}
+
+	if rb.body == nil {
+		c.Status(rb.status)
+		return nil
+	}
+
+	switch body := rb.body.(type) {
+	case []byte:
+		c.Data(rb.status, "application/octet-stream", body)
+	case string:
+		c.String(rb.status, body)
+	default:
+		c.JSON(rb.status, body)
+	}
+
+	return nil
+}
+
+func Ok(body any) *ResponseBuilder {
+	return Response().Status(http.StatusOK).Body(body)
+}
+
+func Created(location string) *ResponseBuilder {
+	return Response().Status(http.StatusCreated).Header("Location", location)
+}
+
+func Accepted() *ResponseBuilder {
+	return Response().Status(http.StatusAccepted)
+}
+
+func NoContent() *ResponseBuilder {
+	return Response().Status(http.StatusNoContent)
+}
+
+func HTML(body string) *ResponseBuilder {
+	return Response().
+		Header("Content-Type", "text/html; charset=utf-8").
+		Body(body)
+}
+
+func JSON(status int, body any) *ResponseBuilder {
+	return Response().
+		Status(status).
+		Body(body)
 }
