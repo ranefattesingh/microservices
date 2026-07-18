67f9c52e06bdbbc8e0b36cd68aaa5226a91aa6e8
Fattesingh Rane <ranefattesingh@hotmail.com>
2026-07-18 14:42:48 +0000

    fix: pkg go module

--- users/internal/router/router.go
+++ users/internal/router/router.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
-	"github.com/ranefattesingh/ecommerce-platform/users/pkg/response"
+	"github.com/ranefattesingh/ecommerce-platform/pkg/response"
 )
 
 type Router interface {
